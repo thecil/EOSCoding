6347416aeb2d3663507c114134f9e9c767676ceb
thecil <53628989+thecil@users.noreply.github.com>
2020-03-22 20:37:20 -0400

    Update dogcontractTOKEN.cpp

--- dogcontractTOKEN.cpp
+++ dogcontractTOKEN.cpp
@@ -7,7 +7,8 @@ using namespace eosio;
 CONTRACT dogcontract : public contract {
   public:
     using contract::contract;
-    dogcontract(name receiver, name code, datastream<const char*> ds):contract(receiver, code, ds)
+    dogcontract(name receiver, name code, datastream<const char*> ds)
+    :contract(receiver, code, ds)
      , currency_symbol("DOGCOIN", 0){}
     /*
     ** Function insert the name and age of a dog with the dog_id.
@@ -16,6 +17,11 @@ CONTRACT dogcontract : public contract {
     ACTION insert(name owner, std::string dog_name, int age){
       //Require auth of the owner
       require_auth(owner);
+      //check balance of sender/owner_index
+      check_balance(owner);
+      //reduce balance
+      reduce_balance(owner);
+
       //Get the index of our table
       dog_index dogs(get_self(), get_self().value); //this is an scope
       //Execute the insert function. Specifying the dog_name and age,
@@ -137,4 +143,28 @@ CONTRACT dogcontract : public contract {
       typedef multi_index<"dogs"_n, dog, indexed_by<"byowner"_n, const_mem_fun<dog, uint64_t, &dog::by_owner>>> dog_index;
       //index for balance table
       typedef multi_index<"balances"_n, balance> balance_index;
+
+      //check balance for owner
+      void check_balance(name user){
+        //scope to get balance value
+        balance_index balances(get_self(), user.value);
+        //get balance from account, if empty throw error msg
+        auto row = balances.get(currency_symbol.raw(), "DOGCOIN balance is empty.");
+        //check
+        check(row.funds.amount >= 10, "Not enough DOGCOINS Deposited.");
+      };//end check balance
+
+      void reduce_balance(name user){
+        //scope to get balance value
+        balance_index balances(get_self(), user.value);
+        //find currency symbol balance
+        auto iterator = balances.find(currency_symbol.raw());
+        //if there is a balance in account
+        if(iterator != balances.end()){
+          //charge/reduce balance for action performed
+          balances.modify(iterator, get_self(), [&](auto &row){
+            row.funds.set_amount(row.funds.amount-10);
+          });
+        }//end if
+      };//end reduce_balance
 };
